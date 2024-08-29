64de852dcd5b3446aafb4330c9d0c4c143301d79
Avadhut Jagde <avadhutjagde555@gmail.com>
2024-08-29 22:19:10 +0530

    [Course: 1. React 18 for Beginners > 3-Building-Components (1h) ] [ Video: #14-Exercise-Showing-an-Alert_mp4  ] - on button click show alert and on close close Alert.

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,14 +1,20 @@
+import { useState } from "react";
 import Button from "./components/Button";
+import Alert from "./components/Alert";
 
 function App() {
-  
+  const [alertVisible, SetAlertVisible] = useState(false);
+
   return (
     <div>
-       <Button color="primary" onClick={() => console.log("Clicked...") }>My Button</Button>
+      {alertVisible && (
+        <Alert onClose={() => SetAlertVisible(false)}>My Alert</Alert>
+      )}
+      <Button color="primary" onClick={() => SetAlertVisible(true)}>
+        My Button
+      </Button>
     </div>
   );
 }
 
 export default App;
-
-
