ce999e15ab79d45347cc1cb3763c762499f02fce
Avadhut Jagde <avadhutjagde555@gmail.com>
2024-08-29 19:46:53 +0530

    [Course: 1. React 18 for Beginners > 3-Building-Components (1h) ] [ Video: #11-Passing-Children_mp4 ] - using  prop we pass HTML content from '<App>' component (Parent-component) To  '<Alert>' component (Child-component).

--- react-app/README.md
+++ react-app/README.md
@@ -2,4 +2,7 @@
 
 # Commit message format : 
 
-[Course: 1. React 18 for Beginners > 3-Building-Components (1h) ] [ Video: #9-Passing-Functions-via-Props_mp4 ] - Parent to Child "Function" pass through `props` so, that Child to parent Data can be Transfer. 
+[Course: 1. React 18 for Beginners > 3-Building-Components (1h) ] [ Video: #11-Passing-Children_mp4 ] - using `children` prop we pass HTML content from `<App>` component (Parent-component) To  `<Alert>` component (Child-component).
+
+
+
--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,21 +1,14 @@
-import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
 
 function App() {
-  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
-
+  
   return (
     <div>
-      <ListGroup
-        items={items}
-        heading="Cities"
-        onSelectItem={handleSelectItem}
-      />
+       <Alert> Hello  <h1> world </h1> </ Alert> 
     </div>
   );
 }
 
 export default App;
+
+
