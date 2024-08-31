da365e93f0979fda507cb3c3bc8443425e6bc782
Avadhut Jagde <avadhutjagde555@gmail.com>
2024-08-31 16:29:43 +0530

    [Course: 1. React 18 for Beginners > 4-Styling Components (32m) ] [ Video: #8-Adding-Icons_mp4  ] - React icons : import { BsFillCalendarFill } from 'react-icons';

--- react-app/README.md
+++ react-app/README.md
@@ -2,7 +2,7 @@
 
 # Commit message format : 
 
-[Course: 1. React 18 for Beginners > 4-Styling Components (32m) ] [ Video: #6-Inline-Styles_mp4 ] - Inline-Style : <ul className="list-group" style={{ backgroundColor: 'yellow'}}>" 
+[Course: 1. React 18 for Beginners > 4-Styling Components (32m) ] [ Video: #8-Adding-Icons_mp4  ] - React icons : import { BsFillCalendarFill } from 'react-icons';
 
 
 
--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -2,14 +2,14 @@ import { useState } from "react";
 import Button from "./components/Button";
 import Alert from "./components/Alert";
 import ListGroup from "./components/ListGroup";
-
+import { BsFillCalendarFill } from 'react-icons/bs';
 
 function App() {
  const items = ["New york" , "Los Angles", "San Francisco"]
 
   return (
     <div>
-      <ListGroup heading="Miami" items={items} onSelectItem={() => console.log("Hello...")}/>
+      <BsFillCalendarFill color="red" size={40} />
     </div>
   );
 }
