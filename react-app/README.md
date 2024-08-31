d2445c04443aa3295c2e4c35ad9bf45f1a45e774
Avadhut Jagde <avadhutjagde555@gmail.com>
2024-08-31 15:07:03 +0530

    [Course: 1. React 18 for Beginners > 4-Styling Components (32m) ] [ Video: #6-Inline-Styles_mp4 ] - Inline-Style : <ul className=list-group style={{ backgroundColor: 'yellow'}}>

--- react-app/README.md
+++ react-app/README.md
@@ -2,7 +2,7 @@
 
 # Commit message format : 
 
-[Course: 1. React 18 for Beginners > 4-Styling Components (32m) ] [ Video: #4-CSS-in-JS_mp4 ] - in ListGroup.tsx component create 'react component' using 'styled-components' Library and replace regular HTML element with that 'react component'. 
+[Course: 1. React 18 for Beginners > 4-Styling Components (32m) ] [ Video: #6-Inline-Styles_mp4 ] - Inline-Style : <ul className="list-group" style={{ backgroundColor: 'yellow'}}>" 
 
 
 
--- react-app/src/components/ListGroup/ListGroup.tsx
+++ react-app/src/components/ListGroup/ListGroup.tsx
@@ -1,21 +1,5 @@
 import { useState } from "react";
 import "./ListGroup.css";
-import styled from "styled-components";
-
-
-const List = styled.ul`
-    list-style: none;
-    padding: 0;
-`;
-
-interface ListItemProps{
-  active: boolean;
-}
-
-const ListItem = styled.li<ListItemProps>`
-    padding: 5px 0;
-    background: ${(props) => (props.active ? "blue" : "none")};
-`;
 
 interface Props {
   items: string[];
@@ -30,10 +14,12 @@ function ListGroup({ items, heading, onSelectItem }: Props) {
     <>
       <h1>{heading}</h1>
       {items.length === 0 && <p>No item found</p>}
-      <List>
+      <ul className="list-group" style={{ backgroundColor: 'yellow'}}>
         {items.map((item, index) => (
-          <ListItem
-            active={index === selectedIndex}
+          <li
+            className={
+              selectedIndex === index ? "list-group-item active" : "list-group-item"
+            }
             key={item}
             onClick={() => {
               setSelectedIndex(index);
@@ -41,9 +27,9 @@ function ListGroup({ items, heading, onSelectItem }: Props) {
             }}
           >
             {item}
-          </ListItem>
+          </li>
         ))}
-      </List>
+      </ul>
     </>
   );
 }
