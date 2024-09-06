9fa5ba44e022e41b80ea0b9ee45c5bd89af78150
Avadhut Jagde <avadhutjagde555@gmail.com>
2024-09-06 22:20:21 +0530

    [Course: 1. React 18 for Beginners > 5-Managing Component State (42m) ] [ Video: #12-Exercise-Updating-State_mp4_4min_49sec ] - Exercise: 1, 2, and 3.

--- react-app/README.md
+++ react-app/README.md
@@ -2,7 +2,7 @@
 
 # Commit message format : 
 
-[Course: 1. React 18 for Beginners > 5-Managing Component State (42m) ] [ Video: #11-Sharing-State-between-Components_mp4_6min_22sec ] - 2 new components `NavBar` and `Cart` created.
+[Course: 1. React 18 for Beginners > 5-Managing Component State (42m) ] [ Video: #12-Exercise-Updating-State_mp4_4min_49sec ] - Exercise: 1, 2, and 3.
 
 
 
--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,15 +1,110 @@
+// ******************* Solution 1 :
+// import { useState } from "react";
+
+// const App = () => {
+// 	const [game, setGame] = useState({
+// 		id: 1,
+// 		player: {
+// 			name: "John",
+// 		},
+// 	});
+
+// 	const handleClick = () => {
+// 		setGame({ ...game, player: { name: 'Bob' } })
+// 	}
+
+// 	return (
+// 		<div>
+// 			<span>{game.player.name}</span>
+// 			<button onClick={handleClick}>Click Me</button>
+// 		</div>
+// 	);
+// }
+
+// export default App;
+
+
+
+
+// ******************* Best Solution 1 :
+// import { useState } from "react";
+
+// const App = () => {
+// 	const [game, setGame] = useState({
+// 		id: 1,
+// 		player: {
+// 			name: "John",
+// 		},
+// 	});
+
+// 	const handleClick = () => {
+// 		setGame({ ...game, player: { ...game.player, name: 'Bob' }})
+// 	}
+
+// 	return (
+// 		<div>
+// 			<span>{game.player.name}</span>
+// 			<button onClick={handleClick}>Click Me</button>
+// 		</div>
+// 	);
+// }
+
+// export default App;
+
+
+
+
+// ******************* Solution 2 :
+// import { useState } from "react";
+
+// const App = () => {
+// 	const [pizza, setPizza] = useState({
+// 		name: 'Spicy Pepperoni',
+// 		toppings: ['Mushroom'],
+// 	});
+
+// 	const handleClick = () => {
+// 		setPizza({ ...pizza, toppings: [...pizza.toppings, 'Cheese'] })
+// 	}
+
+// 	return (
+// 		<div>
+// 			<span>{pizza.toppings}</span>
+// 			<button onClick={handleClick}>Click Me</button>
+// 		</div>
+// 	);
+// }
+
+// export default App;
+
+
+
+// ******************* Solution 3 :
 import { useState } from "react";
-import NavBar from "./components/NavBar";
-import Cart from "./components/Cart";
 
 const App = () => {
-	const [cartItems, setCartItems] = useState([ 'product1', 'product2' ])
-	
+	const [cart, setCart] = useState({
+		discount: .1,
+		items: [
+			{ id: 1, title: 'Product 1', quantity: 1 },
+			{ id: 2, title: 'Product 1', quantity: 1 }
+		],
+	});
+
+	const handleClick = () => {
+		setCart({ 
+			...cart, 
+			items: cart.items.map(item => 
+			  item.id == 1 ? { ...item, quantity: item.quantity + 1 } : item
+			) 
+		  })
+	}
+
 	return (
-	<div>
-		<NavBar cartItemsCount={cartItems.length} />
-    	<Cart cartItems={cartItems}  onClear={() => setCartItems([]) } />
-	</div>
+		<div>
+			<span>{cart.items.map((item) => `Object-${item.id} "${item.quantity}" `)}</span>
+			<button onClick={handleClick}>Click Me</button>
+		</div>
 	);
 }
 
