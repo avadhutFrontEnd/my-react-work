04dae60bfa36ab0eadf0bbd752d4e8c8964dbf7c
Avadhut Jagde <avadhutjagde555@gmail.com>
2024-09-08 00:04:06 +0530

    [Course: 1. React 18 for Beginners > 6-Building Forms (71m) ] [ Video: #5-Controlled-Components_mp4_4min_33sec ] -another way to get the  of input fields in a form. Instead of the  hook, we can use the  hook().

--- react-app/README.md
+++ react-app/README.md
@@ -2,7 +2,7 @@
 
 # Commit message format : 
 
-[Course: 1. React 18 for Beginners > 6-Building Forms (71m) ] [ Video: #4-Accessing-Input-Fields_mp4_5min_46sec ] - useRef Hook, `ref.current.value` property and while initializing useRef() why their is a need to initialize useRef() with `null` like this useRef(null).
+[Course: 1. React 18 for Beginners > 6-Building Forms (71m) ] [ Video: #5-Controlled-Components_mp4_4min_33sec ] -another way to get the `value` of input fields in a form. Instead of the `ref` hook, we can use the `state` hook(`useState`).  
 
 
 
--- react-app/src/components/Form.tsx
+++ react-app/src/components/Form.tsx
@@ -1,22 +1,12 @@
-import React, { FormEvent, useRef } from "react";
+import React, { FormEvent, useRef, useState } from "react";
 
 const Form = () => {
+  const [person, setPerson] = useState({ name: "", age: '' });
 
-    const nameRef = useRef<HTMLInputElement>(null);
-    const ageRef = useRef<HTMLInputElement>(null);
-    const person = { name: '', age: 0 };
-
-    const handleSubmit = (event: FormEvent) => {
-        event.preventDefault()
-
-        if (nameRef.current !== null) {
-            person.name = nameRef.current.value;
-        }
-        if (ageRef.current !== null) {
-            person.age = parseInt(ageRef.current.value);
-        }
-        console.log(person);
-    }
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+    console.log(person);
+  };
 
   return (
     <form action="" onSubmit={handleSubmit}>
@@ -24,13 +14,33 @@ const Form = () => {
         <label htmlFor="name" className="form-label">
           Name
         </label>
-        <input ref={nameRef} id="name" type="text" className="form-control" />
+        <input
+          onChange={(event) =>
+            setPerson({ ...person, name: event.target.value })
+          }
+          value={person.name}
+          id="name"
+          type="text"
+          className="form-control"
+        />
       </div>
       <div className="mb-3">
-        <label htmlFor="age" className="form-label">Age</label>
-        <input ref={ageRef} id="age" type="number" className="form-control" />
+        <label htmlFor="age" className="form-label">
+          Age
+        </label>
+        <input
+          onChange={(event) =>
+            setPerson({ ...person, age: event.target.value })
+          }
+          value={person.age}
+          id="age"
+          type="number"
+          className="form-control"
+        />
       </div>
-      <button className="btn btn-primary" type="submit">Submit</button>
+      <button className="btn btn-primary" type="submit">
+        Submit
+      </button>
     </form>
   );
 };
