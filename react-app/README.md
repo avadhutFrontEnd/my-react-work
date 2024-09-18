9f1fbe7cf7dae8877604d4b08c090b009f92bfb1
Avadhut Jagde <avadhutjagde555@gmail.com>
2024-09-18 21:57:58 +0530

    [Course: 1. React 18 for Beginners > 6-Building Forms (71m) ] [ Video: #11-Building-ExpenseList_mp4_10min_28sec ] - creating `ExpenseList.tsx` component.

--- react-app/README.md
+++ react-app/README.md
@@ -2,6 +2,6 @@
 
 # Commit message format : 
 
-[Course: 1. React 18 for Beginners > 6-Building Forms (71m) ] [ Video: #9-Disabling-the-Submit-Button_mp4_45sec ] - grabbing the `isValid` from `formState`.
+[Course: 1. React 18 for Beginners > 6-Building Forms (71m) ] [ Video: #11-Building-ExpenseList_mp4_10min_28sec ] - creating `ExpenseList.tsx` component.
 
 
--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import Form from './components/Form'
+import React, { useState } from 'react'
+import ExpenseList from './expense-tracker/components/ExpenseList'
 
 const App = () => {
+	const [expenses, setExpenses] = useState([
+		{id:1, description:'aaa', amount: 10, category: 'Utilities'},
+		{id:2, description:'bbb', amount: 10, category: 'Utilities'},
+		{id:3, description:'ccc', amount: 10, category: 'Utilities'},
+		{id:4, description:'ddd', amount: 10, category: 'Utilities'},
+	]);
+
   return (
 	<div>
-		<Form />
+		<ExpenseList expenses={expenses} onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))} />
 	</div>
   )
 }
