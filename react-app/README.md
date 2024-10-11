2d221cf4e2cb1276ac0bf6c3f6bbf080bd4830aa
Avadhut Jagde <avadhutjagde555@gmail.com>
2024-10-11 19:45:40 +0530

    [Course: 1. React 18 for Beginners > 6-Building Forms (71m) ] [ Video: #4-Effect-Clean-Up_mp4_2min_30sec ] - our  function should stop or undo whatever the effect was doing.

--- react-app/README.md
+++ react-app/README.md
@@ -2,6 +2,6 @@
 
 # Commit message format : 
 
-[Course: 1. React 18 for Beginners > 6-Building Forms (71m) ] [ Video: #3-Effect-Dependencies_mp4_8min_34sec  ] - created `ProductList.tsx` component.
+[Course: 1. React 18 for Beginners > 6-Building Forms (71m) ] [ Video: #4-Effect-Clean-Up_mp4_2min_30sec ] - our `cleanup` function should stop or undo whatever the effect was doing.
 
 
--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,21 +1,17 @@
-// App.tsx
-
 import { useEffect } from "react";
-import { useState } from "react";
-import ProductList from "./components/ProductList";
+
+const connect = () => console.log('Connecting');
+const disconnect = () => console.log('Disconnecting');
 
 function App() {
-	const [category, setCategory] = useState('');
+	useEffect( () => {
+		connect();
+		 
+		return () => disconnect();
+	});
 	
 	return (
-		<div>
-			<select className="form-select" onChange={(event) => setCategory(event.target.value)}>
-		    <option value=""></option>
-		    <option value="Clothing">Clothing</option>
-		    <option value="Household">Household</option>
-		  </select>
-			<ProductList category={category} />
-		</div>
+		<div></div>
 	);
 }
 
