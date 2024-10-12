fe792d8009d72b1f89af2816ad80f7b55e2399f9
Avadhut Jagde <avadhutjagde555@gmail.com>
2024-10-12 11:32:12 +0530

    [Course: 1. React 18 for Beginners > 6-Building Forms (71m) ] [ Video: #8-Working-with-Async-and-Await_mp4_4min_21sec ] - In JavaScript, if you have a promise, we can put the  keyword in front of the promise to get the result.

--- react-app/README.md
+++ react-app/README.md
@@ -2,5 +2,6 @@
 
 # Commit message format : 
 
-[Course: 1. React 18 for Beginners > 6-Building Forms (71m) ] [ Video: #7-Handling-Errors_mp4_2min_03sec ] - In JavaScript, all promises have a method called `catch` that we can use for catching errors. This `catch` callback function will get executed if something goes wrong while fetching the data.
+[Course: 1. React 18 for Beginners > 6-Building Forms (71m) ] [ Video: #8-Working-with-Async-and-Await_mp4_4min_21sec ] - In JavaScript, if you have a promise, we can put the `await` keyword in front of the promise to get the result.
+
 
--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,31 +1,49 @@
 // App.tsx
-import axios from 'axios';
-import { useState, useEffect } from 'react';
+import axios, { AxiosError } from "axios";
+import { useEffect, useState } from "react";
 
 interface User {
-	id: number;
-	name: string;
+  id: number;
+  name: string;
 }
 
 function App() {
-	const [ users, setUsers ] = useState<User[]>([]);
-	const [ error, setError ] = useState('');
+  const [users, setUsers] = useState<User[]>([]);
+  const [error, setError] = useState("");
 
-	useEffect( () => {
-		axios.get<User[]>('https://jsonplaceholder.typicode.com/xusers')
-		.then( res => setUsers(res.data))
-		.catch(err => setError(err.message));
+  // Option 1 : `then()` and `catch()` method implementation :
+  // useEffect( () => {
+  // 	axios.get<User[]>('https://jsonplaceholder.typicode.com/xusers')
+  // 	.then( res => setUsers(res.data))
+  // 	.catch(err => setError(err.message));
 
-	}, []);
-	
-	return  (
-	<>
-		{ error && <p className="text-danger">{error}</p> }
-		<ul>
-			{users.map( user => <li key={user.id}>{user.name}</li> )}
-		</ul>
-	</>
-	);
+  // }, []);
+
+
+  // Option 2 :
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const res = await axios
+          .get<User[]>("https://jsonplaceholder.typicode.com/users");
+        setUsers(res.data);
+      } catch (err) {
+        setError((err as AxiosError).message);
+      }
+    };
+
+    fetchUsers();
+  }, []);
+
+
+  return (
+    <>
+      {error && <p className="text-danger">{error}</p>}
+      <ul>
+        {users.map((user) => <li key={user.id}>{user.name}</li>)}
+      </ul>
+    </>
+  );
 }
 
 export default App;
